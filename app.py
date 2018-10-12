87faa2c1ce123a05ba0e4588d8d119de6dd91e92
Karl Horning <k.horning@me.com>
2018-10-12 17:20:44 +0100

    Added the option to send HTML emails and plain text as an alternative

--- app.py
+++ app.py
@@ -5,15 +5,26 @@ from config import Config
 
 email_from = Config.EMAIL_ADDRESS
 email_to = Config.EMAIL_RECIPIENT
-email_body = 'Hello, World!'
+email_body_text = 'Hello, World!\n\nThis is my test email!'
+email_body_html = """
+<html>
+  <head></head>
+  <body>
+    <p>Hello, World!</p>
+    <p>This is my test email!</p>
+    <p>Here's a <a href="http://www.python.org">link</a>.</p>
+  </body>
+</html>
+"""
 username = Config.USERNAME
 password = Config.PASSWORD
 
-email = MIMEMultipart()
+email = MIMEMultipart('alternative')
 email['From'] = email_from
 email['To'] = email_to
 email['Subject'] = 'Test Email'
-email.attach(MIMEText(email_body))
+email.attach(MIMEText(email_body_text, 'plain'))
+email.attach(MIMEText(email_body_html, 'html'))
 
 # Google
 # server = smtplib.SMTP('smtp.gmail.com:587')
